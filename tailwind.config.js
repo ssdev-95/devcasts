e5413f879dbacb988009690d0b21d4b39de83f42
Salomao Souza <souza95salomao@gmail.com>
2021-04-19 09:48:10 -0400

    [UPDATE] Files tree structure

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      theme: {
+        colorrs: {}
+      },
+      fontFamily: {
+        'inter': 'Inter'
+      }
+    },
+  },
+  plugins: [],
+}
